17b4626bec38624d2e8ee1458051473faf30bfd6
Michael Jester <m.jester.93@gmail.com>
2020-09-18 18:02:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+  let minimum = array[0]
+  let index = 0
 
+  for (let i=1; i<array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i]
+      index = i
+    }
+  }
+
+  return array.splice(index, 1)[0]
 }
 
 function selectionSort(array){
+  let sortedArray = [];
+  let minimum;
 
+  while (array.length !== 0) {
+    sortedArray.push(findMinAndRemove(array))
+  }
+
+  return sortedArray
 }
