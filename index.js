566980299ca125261452b08f30d93ab8a2c9e43c
Michael Jester <m.jester.93@gmail.com>
2020-09-18 17:59:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function findMinAndRemove(array){
   let minimum = array[0]
+  let index = 0
 
   for (let i=1; i<array.length; i++) {
     if (array[i] < minimum) {
       minimum = array[i]
+      index = i
     }
   }
 
-  return minimum
+  return array
 }
 
 function selectionSort(array){
